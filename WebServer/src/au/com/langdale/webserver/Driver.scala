7646fc94b4cb686076976ac445c90f0c00a0edf8
Arnold deVos <adv-contact-github@langdale.com.au>
2010-04-18 07:34:45 +1000

    remove dependency on SLF4J; additional driver methods

--- WebServer/src/au/com/langdale/webserver/Driver.scala
+++ WebServer/src/au/com/langdale/webserver/Driver.scala
@@ -4,15 +4,18 @@ import org.mortbay._
 import jetty.Server
 import jetty.handler.{HandlerCollection, DefaultHandler, AbstractHandler, ContextHandlerCollection}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import log.Log
 
 object Driver {
-  System.setProperty("org.mortbay.log.class","au.com.langdale.webserver.Slf4jLog")
   val server = new Server
   val contexts = new ContextHandlerCollection
   val handlers = new HandlerCollection
   handlers setHandlers Array(requestLogger, contexts, new DefaultHandler)
   server setHandler handlers
+  
   def start = server.start
+  def join = server.join
+  def stop = server.stop
   
   object requestLogger extends AbstractHandler {
     def handle( target: String, request: HttpServletRequest, response: HttpServletResponse, kind: Int) {
